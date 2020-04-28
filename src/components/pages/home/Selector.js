e22a8e95904b599adf25cf5324b5bebe8d8f6953
David Schmidt <david.adam.schmidt@gmail.com>
2020-04-28 23:13:45 +0200

    Use React Select

--- src/components/pages/home/Selector.js
+++ src/components/pages/home/Selector.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 340px;
+  padding: 20px;
+`;
+
+const createOptions = dojos =>
+  dojos.map(d => {
+    return { value: d.id, label: d.title };
+  });
 
 const Selector = ({ dojos, handleSelect }) => {
+  const [options, setOptions] = useState(createOptions(dojos));
+
+  useEffect(() => {
+    setOptions(createOptions(dojos));
+  }, [dojos]);
+
   return (
-    <select
-      defaultValue={"default"}
-      onChange={e => handleSelect(e.target.value)}
-    >
-      <option value={"default"} disabled hidden>
-        Select
-      </option>
-      {dojos.map(dojo => (
-        <option key={dojo.id} value={dojo.id}>
-          {dojo.title}
-        </option>
-      ))}
-    </select>
+    <Wrapper>
+      <Select
+        options={options}
+        onChange={o => handleSelect(o.value)}
+        placeholder="Select a dojo..."
+        noOptionsMessage={() => "No dojo available"}
+        isSearchable
+      />
+    </Wrapper>
   );
 };
 
